c09baa085802cc95b3d9bcc5f027d2d906f97cbe
Sean Griffin <sean@seantheprogrammer.com>
2016-04-14 10:54:54 -0600

    WIP:

--- ext/rust_example/extconf.rb
+++ ext/rust_example/extconf.rb
@@ -1,7 +1,6 @@
 require "mkmf"
 
 have_library("rust_example", "rust_example_init")
-libdir = File.expand_path("../../lib", __FILE__)
-$LOCAL_LIBS << "-L#{libdir} -lstuff "
+$LOCAL_LIBS << "-lstuff"
 
 create_makefile("rust_example/rust_example")
