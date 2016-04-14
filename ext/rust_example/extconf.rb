8e32237155aaad026bd3c4967e90198dc9aa994e
Sean Griffin <sean@seantheprogrammer.com>
2016-04-14 10:51:32 -0600

    WIP

--- ext/rust_example/extconf.rb
+++ ext/rust_example/extconf.rb
@@ -1,5 +1,7 @@
 require "mkmf"
 
 have_library("rust_example", "rust_example_init")
+libdir = File.expand_path("../../lib", __FILE__)
+$LOCAL_LIBS << "-L#{libdir} -lstuff "
 
 create_makefile("rust_example/rust_example")
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,36 @@
 extern crate libc;
 
 use std::ffi::CStr;
-use std::marker::PhantomData;
 use std::{slice, str, mem};
 
 #[repr(C)]
-pub struct RubyStr<'a> {
+pub struct RubyStr {
     data: *const libc::c_char,
     len: libc::c_long,
-    _lifetime: PhantomData<&'a ()>,
 }
 
-impl<'a> RubyStr<'a> {
-    fn as_str(&self) -> &'a str {
+impl RubyStr {
+    fn as_str(&self) -> &str {
         unsafe {
             let bytes = slice::from_raw_parts(self.data as *const u8, self.len as usize);
             str::from_utf8_unchecked(bytes)
         }
     }
 
-    fn from_str(string: &'a str) -> Self {
+    fn from_str(string: &str) -> Self {
         RubyStr {
             data: string.as_ptr() as *const libc::c_char,
             len: string.len() as libc::c_long,
-            _lifetime: PhantomData,
         }
     }
 }
 
 #[no_mangle]
-pub extern "C" fn ruby_str_replace<'a>(
-    haystack: RubyStr<'a>,
-    needle: RubyStr<'a>,
-    replacement: RubyStr<'a>,
-) -> RubyStr<'static> {
+pub extern "C" fn ruby_str_replace(
+    haystack: RubyStr,
+    needle: RubyStr,
+    replacement: RubyStr,
+) -> RubyStr {
     let mut replaced = haystack.as_str().replace(needle.as_str(), replacement.as_str());
     replaced.shrink_to_fit();
     let leaked_str = unsafe { mem::transmute(&*replaced) };
@@ -42,7 +39,7 @@ pub extern "C" fn ruby_str_replace<'a>(
 }
 
 #[no_mangle]
-pub extern "C" fn drop_ruby_str<'a>(ruby_str: RubyStr<'a>) {
+pub extern "C" fn drop_ruby_str(ruby_str: RubyStr) {
     unsafe {
         mem::drop(String::from_raw_parts(
             ruby_str.data as *mut _,
